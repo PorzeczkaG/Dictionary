6ba673015fe08c6519bb3d250bf0fe759a22e36d
PorzeczkaG <64199094+PorzeczkaG@users.noreply.github.com>
2020-04-28 19:39:14 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>(); //Definicja
+            dictionary.Add(1, "Tomasz"); //Pierwsza wartość klucz
+            dictionary.Add(2, "Jan");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
